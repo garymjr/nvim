6d8b060d62a47118948fc2a4a6e93ab71765afc6
Gary Murray <garymjr@gmail.com>
2024-05-22 10:31:48 -0600

    cleanup dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,10 @@
 return {
 	{ "bufferline.nvim", enabled = false },
   { "noice.nvim", enabled = false },
+  {
+    "dashboard-nvim",
+    opts = function(_, opts)
+      opts.config.header = vim.split(string.rep("\n", 15), "\n")
+    end,
+  }
 }
