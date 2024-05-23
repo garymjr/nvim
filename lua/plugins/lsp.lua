950ffba6a21fca8188d790f10f9a8ca3e836b2cf
Gary Murray <garymjr@gmail.com>
2024-05-23 15:17:43 -0600

    add fidget.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,28 @@
 return {
-	"nvim-lspconfig",
-	opts = {
-		document_highlight = {
-			enabled = false,
-		},
-		inlay_hints = {
-			enabled = false,
-		},
-		servers = {
-			elixirls = {
-				settings = {
-					elixirLS = {
-						dialyzerEnabled = false,
+	{
+		"nvim-lspconfig",
+		opts = {
+			document_highlight = {
+				enabled = false,
+			},
+			inlay_hints = {
+				enabled = false,
+			},
+			servers = {
+				elixirls = {
+					settings = {
+						elixirLS = {
+							dialyzerEnabled = false,
+						},
 					},
 				},
 			},
 		},
+	},
+	{
+		"j-hui/fidget.nvim",
+    event = "VeryLazy",
+    dependencies = {"nvim-lspconfig"},
+		opts = {},
 	},
 }
