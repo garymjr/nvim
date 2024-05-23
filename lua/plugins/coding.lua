54598410341c20d62a0cb24535ede93f47cc3503
Gary Murray <garymjr@gmail.com>
2024-05-23 15:17:52 -0600

    clean up cmp mappings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,3 +1,18 @@
 return {
 	{ "mini.pairs", enabled = false },
+	{
+		"nvim-cmp",
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.mapping["<CR>"] = function(fallback)
+				cmp.abort()
+				fallback()
+			end
+			opts.mapping["<S-CR>"] = function(fallback)
+				cmp.abort()
+				fallback()
+			end
+			opts.mapping["<C-y>"] = LazyVim.cmp.confirm()
+		end,
+	},
 }
