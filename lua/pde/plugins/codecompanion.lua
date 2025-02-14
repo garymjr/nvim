284dcfb9d1d72b48cb1512cd913edca852f2c3bd
Gary Murray <garymjr@gmail.com>
2025-02-14 12:15:55 -0700

    feat: update prompt for generating commit messages

--- lua/pde/plugins/codecompanion.lua
+++ lua/pde/plugins/codecompanion.lua
@@ -48,6 +48,36 @@ later(function()
 				provider = "mini_diff",
 			},
 		},
+		prompt_library = {
+			["Generate a Commit Message"] = {
+				strategy = "chat",
+				description = "Generate a commit message",
+				opts = {
+					index = 10,
+					is_default = true,
+					is_slash_cmd = true,
+					short_name = "commit",
+					auto_submit = true,
+				},
+				prompts = {
+					{
+						role = "user",
+						content = function()
+							return string.format(
+								[[Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.
+```diff
+%s
+```]],
+								vim.fn.system("git diff --no-ext-diff --staged")
+							)
+						end,
+						opts = {
+							contains_code = true,
+						},
+					},
+				},
+			},
+		},
 		strategies = {
 			chat = {
 				adapter = "copilot",
