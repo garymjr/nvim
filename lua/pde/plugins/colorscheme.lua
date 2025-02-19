34082fc375753110829fa11ec88c82232f258be5
Gary Murray <garymjr@gmail.com>
2025-02-19 11:16:44 -0700

    feat: Configure LspReferenceText highlight
     This commit configures the LspReferenceText highlight color for
     the poimandres colorscheme to improve readability.

--- lua/pde/plugins/colorscheme.lua
+++ lua/pde/plugins/colorscheme.lua
@@ -4,6 +4,14 @@ add "olivercederborg/poimandres.nvim"
 
 now(function()
   require("poimandres").setup()
+
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "poimandres",
+    callback = function()
+      vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#e4f0fb", bg = "#506477" })
+    end,
+  })
+
   vim.cmd.colorscheme "poimandres"
 end)
 
